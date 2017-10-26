9d469b94a0cc6681813855bff27a4a670b6d2cb7
d0u9 <d0u9.su@outlook.com>
2017-10-26 21:24:24 +0800

    update

--- new_pc_setup/boot.sh
+++ new_pc_setup/boot.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y git zsh gcc g++ automake autoconf curl vim             \
                         libtool libtool-bin autoconf pkg-config unzip          \
-                        libevent-dev openssh-server
+                        libevent-dev openssh-server libncurses5-dev
 
 cd /tmp
 git clone https://github.com/d0u9/private.git
