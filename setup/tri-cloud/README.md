291ae8edaeee12562f93a31693c399c8cb1dd7b1
d0u9 <d0u9.su@outlook.com>
2017-10-27 13:32:38 +0800

    add cloud setup scripts

---
+++ setup/tri-cloud/README.md
@@ -0,0 +1,12 @@
+# tri-cloud
+
+Automatically setup my cloud host.
+
+---
+
+```
+sh -c "$(wget https://raw.githubusercontent.com/d0u9/scripts/master/setup/tri-cloud/boot.sh -O -)"
+```
+---
+
+### ¶ The end
---
+++ setup/tri-cloud/boot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dir="$(pwd)"
+
+# Software update and install
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y git zsh gcc g++ automake autoconf curl vim             \
+                        libtool libtool-bin autoconf pkg-config unzip          \
+                        libevent-dev openssh-server libncurses5-dev
+
+cd /tmp
+git clone https://github.com/d0u9/private.git
+
+git clone https://github.com/d0u9/scripts.git
+
+cd private/setup/tri-cloud
+
+# Run the real setup script
+bash setup.sh
+
+cd "$dir"
+rm -fr /tmp/scripts
+
+echo "------------------------- Done --------------------------"
