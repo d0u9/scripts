11a2c5f4675e6a48a11b7ff1cc3490a495b5d74e
d0u9 <d0u9.su@outlook.com>
2018-04-12 00:23:45 +0800

    add script for backing up photos

---
+++ tri-client/backup_photos_to_extern_usb_disk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rsync_min_version=3.1.1
+
+rsync_version=$(rsync --version | awk '$1=="rsync" && $2=="version" {print $3}')
+c=$(printf "$rsync_version\n$rsync_min_version" | sort -V | head -n1)
+
+if [ "$c" != "$rsync_min_version" ]; then
+    echo "Please update your rsync"
+    exit 1
+fi
+
+rsync -avP --delete --info=progress2 /Volumes/U/Photo /Volumes/FILES_BACKUP/
